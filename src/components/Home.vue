3229a8525ade56c950b31dabf18f7becea6d845e
Henrique <hiquesc_@hotmail.com>
2020-07-23 21:29:13 -0300

    interface gráfica

---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined 
+        multiple 
+        chips 
+        v-model="files" 
+        @click:append-outer="proccessSubtitles" />
+    </v-form>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name"
+        :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+  components: { Pill },
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [
+        { name: 'You', amount: 1234}
+      ]
+    }
+  },
+  methods: {
+    proccessSubtitles() {
+      console.log(this.files)
+    }
+  }
+}
+</script>
+
+<style>
+  .pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
